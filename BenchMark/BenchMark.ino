29a2af65fdfc47fe89a77321662e3c327bb8280f
Takao Akaki <mongonta@gmail.com>
2019-04-18 19:06:37 +0900

    Update BenchMark

--- BenchMark/BenchMark.ino
+++ BenchMark/BenchMark.ino
@@ -5,9 +5,10 @@
 
 String filepath = "/benchmark";
 
-void getWriteTime(fs::FS &fs, int size, int loop)
+int benchmarkMode = 1;
+
+volatile void getWriteTime(fs::FS &fs, int size, int loop)
 {  
-  Serial.printf("#FileSize %dB:Write\n", size);
   int buffer = 4096;
   int fileloop = size / buffer;
 
@@ -17,7 +18,8 @@ void getWriteTime(fs::FS &fs, int size, int loop)
   File f;
   for (int i = 0; i < buffer; ++i) { buf[i] = uint8_t(0xFF); }
   
-  M5.Lcd.printf("Writing %5uB:", size);
+  Serial.printf("Write,%u,", size / 1000);
+  M5.Lcd.printf("Writing %5u KB :", size / 1000);
   for ( int i = 0; i < loop; ++i) {
     M5.Lcd.print(".");
     f = fs.open(filepath + String(i), FILE_WRITE);
@@ -29,15 +31,14 @@ void getWriteTime(fs::FS &fs, int size, int loop)
     res = micros() - start;
     sum += res;
     f.close();
-    Serial.printf("%4u KB/s ", uint32_t((uint64_t)(size * 1000) / res));
+    Serial.printf("%u,", uint32_t((uint64_t)(size * 1000) / res));
   }
-  Serial.printf(": Avr %4u KB/s\n\n", uint32_t((uint64_t)(size * loop * 1000) /sum));
-  M5.Lcd.printf(": Avr %4u KB/s\n", uint32_t((uint64_t)(size * loop * 1000) /sum));
+  Serial.printf("%u\n", uint32_t((uint64_t)(size * loop * 1000) /sum));
+  M5.Lcd.printf(": Avr %5u KB/s\n", uint32_t((uint64_t)(size * loop * 1000) /sum));
 }
 
-void getReadTime(fs::FS &fs, int size, int loop)
+volatile void getReadTime(fs::FS &fs, int size, int loop)
 {  
-  Serial.printf("#FileSize %dB:Read\n", size);
   int buffer = 4096;
   int fileloop = size / buffer;
 
@@ -46,7 +47,8 @@ void getReadTime(fs::FS &fs, int size, int loop)
   uint64_t sum = 0;
   File f;
 
-  M5.Lcd.printf("Reading %5uB:", size);
+  Serial.printf("Read,%u,", size / 1000);
+  M5.Lcd.printf("Reading %5u KB :", size / 1000);
   for ( int i = 0; i < loop; ++i) {
     M5.Lcd.print(".");
     f = fs.open(filepath + String(i), FILE_READ);
@@ -58,10 +60,10 @@ void getReadTime(fs::FS &fs, int size, int loop)
     res = micros() - start;
     sum += res;
     f.close();
-    Serial.printf("%4u KB/s ", uint32_t((uint64_t)(size * 1000) / res));
+    Serial.printf("%u,", uint32_t((uint64_t)(size * 1000) / res));
   }
-  Serial.printf(": Avr %4u KB/s\n\n", uint32_t((uint64_t)(size * loop * 1000) /sum));
-  M5.Lcd.printf(": Avr %4u KB/s\n", uint32_t((uint64_t)(size * loop * 1000) /sum));
+  Serial.printf("%u\n", uint32_t((uint64_t)(size * loop * 1000) /sum));
+  M5.Lcd.printf(": Avr %5u KB/s\n", uint32_t((uint64_t)(size * loop * 1000) /sum));
 }
 
 void removeFile(fs::FS &fs, int loop) {
@@ -70,21 +72,43 @@ void removeFile(fs::FS &fs, int loop) {
   }
 }
 
-void benchmarkStart(fs::FS &fs) {
+void benchmarkStart(fs::FS &fs ,String fsstr) {
   int size = 4096;
-  int loop = 5;
+  int loop = 3;
+
+  int filesize = 0;
+
+  dispTitle();
+
+  M5.Lcd.print(fsstr + " Bench Start!  \n");
+  Serial.print(fsstr + " Bench Start---------------------------------\n");
+
+  Serial.print("Read/Write,Filesize(KB),");
+  for (int i = 0; i < loop; i++) {
+    Serial.printf("loop%u(KB/sec),", i);
+  }
+  Serial.println("average(KB/sec)");
   
-  for (int i = 1; i < 9; i++) {
-    getWriteTime(fs, size * i, loop);
-    getReadTime(fs, size * i, loop);
+  for (int i = 1; i < 10; i++) {
+    if (benchmarkMode == 1) {
+      filesize = size * i ;
+    } else {
+      filesize = size * i * i;
+    }
+    getWriteTime(fs, filesize, loop);
+    getReadTime(fs, filesize, loop);
     removeFile(fs, loop);
   }
+
+  M5.Lcd.print(fsstr + " Bench Finished\n");
+  Serial.print(fsstr + " Bench Finished\n");
 }
 
 void dispTitle() {
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.clear();
-  M5.Lcd.println("Press Button");
+  M5.Lcd.printf("Press Button : mode = %u\n", benchmarkMode);
+  M5.Lcd.println("Button A:Mode Change 1:4-36KB 2:4-331KB");
   M5.Lcd.println("Button B:SPIFFS Benchmark");
   M5.Lcd.println("Button C:SD     Benchmark");
 }
@@ -104,20 +128,22 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   M5.update();
-  if (M5.BtnB.wasPressed()) {
+  if (M5.BtnA.wasPressed()) {
+    if (benchmarkMode == 1) {
+      // filesize 4096 * loop * loop
+      benchmarkMode = 2;
+    } else {
+      // fileseize 4096 * loop
+      benchmarkMode = 1;
+    }
     dispTitle();
-    M5.Lcd.print("SPIFFS Bench Start!  \n");
-    Serial.print("SPIFFS Bench \n");
+  }
+  if (M5.BtnB.wasPressed()) {
     SPIFFS.begin();
-    benchmarkStart(SPIFFS);
-    M5.Lcd.print("SPIFFS Bench Finished\n");
+    benchmarkStart(SPIFFS, "SPIFFS");
   }
   if (M5.BtnC.wasPressed()) {
-    dispTitle();
-    M5.Lcd.print("SD Bench Start!      \n");
-    Serial.print("SD Bench \n");
-    benchmarkStart(SD);
-    M5.Lcd.print("SD Bench Finished    \n");
+    benchmarkStart(SD, "SD");
   }
 
 }
